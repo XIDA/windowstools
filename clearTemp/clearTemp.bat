4f3a1f78e9c31805a96efcbb9bc5225b14c02cfc
BenjaminKobjolke <b.kobjolke@xida.de>
2014-04-20 18:25:25 +0200

    added clearTemp bat

---
+++ clearTemp/clearTemp.bat
@@ -0,0 +1,26 @@
+REM downloaded files
+del C:\Users\%USERNAME%\Downloads\*.* /F /S /Q
+
+REM delete temp files of different programs
+del C:\Users\%USERNAME%\AppData\Local\Temp\*.* /F /S /Q
+del "C:\Users\%USERNAME%\AppData\Local\Microsoft\Windows\Explorer\*.*" /F /S /Q
+del C:\Users\%USERNAME%\AppData\Local\Microsoft\Windows\WER\*.* /F /S /Q
+
+
+del C:\Users\%USERNAME%\Pictures\Screenpresso\*.* /F /S /Q
+
+del "C:\Users\admin-sama\AppData\Roaming\Microsoft\Windows\Cookies\Cache\*.*" /F /S /Q
+del C:\ProgramData\Microsoft\Windows\WER\*.* /F /S /Q
+
+REM wamp webserver temp files
+del E:\wamp\tmp\*.* /F /S /Q
+
+REM unused
+REM del "C:\Users\%USERNAME%\AppData\Roaming\Adobe\Premiere Pro\5.0\Metadata Caches\*.*" /F /S /Q
+REM del "C:\Users\%USERNAME%\Documents\Adobe\After Effects CS4\Media Cache Files\*.*" /F /S /Q
+REM del "C:\Users\%USERNAME%\AppData\Local\Autodesk\3dsMaxDesign\2010 - 64bit\enu\temp\*.*" /F /S /Q
+REM del "C:\Users\%USERNAME%\AppData\Local\Autodesk\3dsMax\2010 - 64bit\enu\temp\*.*" /F /S /Q
+REM del "C:\Users\%USERNAME%\AppData\Local\Autodesk\3dsMax\2010 - 64bit\enu\temp\*.*" /F /S /Q
+
+
+
