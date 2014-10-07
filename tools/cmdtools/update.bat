d7085649c2f454ac234bb9d6726750efe8d97c70
BenjaminKobjolke <spamprotect@xida.de>
2014-10-07 09:05:13 +0200

    added wget

--- tools/cmdtools/update.bat
+++ tools/cmdtools/update.bat
@@ -1,6 +1,10 @@
 IF exist tools ( echo ... ) ELSE ( mkdir tools )
 del tools\*.*  /F /Q
 bin\curl -L -o tools\kpcli.exe http://downloads.sourceforge.net/project/kpcli/kpcli-2.7.exe
+
+rmdir /Q /S tools\curl
 bin\curl -L -o tools\curl.zip http://curl.haxx.se/gknw.net/7.34.0/dist-w32/curl-7.34.0-rtmp-ssh2-ssl-sspi-zlib-idn-static-bin-w32.zip
-bin\7za.exe e tools\curl.zip -otools\curl
+bin\7za.exe e tools\curl.zip -otools\curl -aoa
 del tools\curl.zip /F /Q
+
+bin\curl -L -o tools\wget.exe http://users.ugent.be/~bpuype/cgi-bin/fetch.pl?dl=wget/wget.exe
